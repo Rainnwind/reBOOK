2785ba20f7ac1e585dea3efa5853924acd95f9ff
Rane Eriksen <re@shipfeed.dk>
2015-10-19 01:00:32 +0200

    update

--- web/app/components/comp-notification/controller.js
+++ web/app/components/comp-notification/controller.js
@@ -10,6 +10,7 @@ APP.directive("compNotification", [function() {
             notification: "=notification"
         },
         controller: ["$scope", "_notifications", function($scope, _notifications) {
+            $scope.outer_class = "alert-danger";
             $scope.class = "error";
             $scope.message = $scope.notification.message;
             $scope.id = $scope.notification.id;
@@ -22,19 +23,42 @@ APP.directive("compNotification", [function() {
             (function init() {
                 switch ($scope.notification.type) {
                     case "ERROR":
+                        $scope.outer_class = "alert-danger";
                         $scope.class = "error";
                         $scope.icon = "fa-ban";
                         break;
                     case "SUCCESS":
+                        $scope.outer_class = "alert-success";
                         $scope.class = "success";
                         $scope.icon = "fa-check-circle";
                         break;
                     case "INFO":
+                        $scope.outer_class = "alert-info";
                         $scope.class = "info";
                         $scope.icon = "fa-info-circle";
                         break;
                 }
             })();
         }],
+        /**
+         *  Setting the element to fadeout once started and adding handlers to take care of
+         *  mousemove (Refresh the message and make it clear)
+         *  and mouseleave to restart the timer that fades the element out again
+         */
+        link: function(scope, element) {
+            var box = $(element).find(".notification-box");
+            var seconds = 10;
+            $(box).fadeOut(1000 * seconds, function() {
+                scope.remove();
+            });
+            $(box).mousemove(function() {
+                $(this).css("opacity", 1).stop();
+            });
+            $(box).mouseleave(function() {
+                $(this).fadeOut(1000 * seconds, function() {
+                    scope.remove();
+                });
+            });
+        }
     };
 }]);
