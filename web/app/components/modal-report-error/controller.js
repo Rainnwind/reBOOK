f87d55e5f60e818afcb8000c1451566b4e5f24b3
Rane Eriksen <re@shipfeed.dk>
2015-10-24 00:05:08 +0200

    Created functionality to handle Full name and email in a proper way - Also made reporting and error go through _bugs

--- web/app/components/modal-report-error/controller.js
+++ web/app/components/modal-report-error/controller.js
@@ -1,44 +1,57 @@
 var APP = angular.module("APP");
-APP.directive("modalReportError", ["_notifications", "$http", function(_notifications, $http) {
+APP.directive("modalReportError", ["_notifications", "$http", "_bugs", function(_notifications, $http, _bugs) {
     return {
         templateUrl: "components/modal-report-error/template.html",
         controller: ["$scope", "_user", "_notifications", function($scope, _user, _notifications) {
-
             //Importing user from _user
             $scope.user = _user.user;
+            $scope.submitting = false;
+
+            var reset_info = function() {
+                $scope.info = {
+                    full_name: ($scope.user.first_name || "").trim() + " " + ($scope.user.last_name || "").trim(),
+                    email: $scope.user.email,
+                    subject: "",
+                    report: "",
+                    contact_later: false,
+                    display_on_site: false
+                };
+            };
 
             //Listening for change on user.id - Making sure that the modal is always up to date with full name and email
             $scope.$watch("user.id", function(v) {
                 if (v) {
-                    $scope.info.full_name = ($scope.user.first_name || "").trim() + " " + ($scope.user.last_name || "").trim();
-                    $scope.info.email = $scope.user.email;
+                    reset_info();
                 }
             });
 
             //Actual data sent to back-end
-            $scope.info = {
-                full_name: "",
-                email: "",
-                report: "",
-                contact_later: false,
-                display_on_site: true
-            };
+            $scope.info = {};
 
+
+            //Submit report error modal to back-end
             $scope.submit = function() {
+                if ($scope.submitting) {
+                    _notifications.ERROR("Please only submit one at a time");
+                    return;
+                }
+
+                $scope.submitting = true;
                 _notifications.INFO("Sending your report!");
-                $http({
-                        url: "/api_open/bugs",
-                        method: "POST",
-                        data: $scope.info
-                    })
+                _bugs.submit($scope.info)
                     .then(function(result) {
-                        console.log(result);
-                        _notifications.handle_success(result.data.notifications);
+                        $("#modal-report-error").modal("hide");
+                        //Resetting the bug report modal
+                        reset_info();
                     })
-                    .catch(function(err) {
-                        _notifications.handle_error(err.data.notifications);
+                    .finally(function() {
+                        $scope.submitting = false;
                     });
             };
+
+            (function init() {
+                reset_info();
+            })();
         }],
         link: function(scope, element) {
             var modal = $(element).find(".modal-dialog");
