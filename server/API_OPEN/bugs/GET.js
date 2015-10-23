8d9f87006e28cc45f8673f8de9ed62bbb722a8cc
Rane Eriksen <re@shipfeed.dk>
2015-10-24 00:11:52 +0200

    API now returns content from server - Open to anyone

--- server/API_OPEN/bugs/GET.js
+++ server/API_OPEN/bugs/GET.js
@@ -1,4 +1,27 @@
 var DB_BUGS = require(process.env.APP_DB_BUGS);
 module.exports = function(_request, _response) {
+    DB_BUGS.find({
+        display_on_site: true
+    }, function(err, bugs) {
+        if (!err) {
+            var _bugs = [];
+            _response
+                ._response
+                ._DATA("bugs", _bugs);
 
+            bugs.forEach(function(bug) {
+                _bugs.push(bug.toJSON());
+            });
+            _response.
+            _response
+                ._send();
+        } else {
+            console.trace(err);
+            _response.
+            _response
+                ._ERROR("Unknown error")
+                ._STATUS(500)
+                ._send();
+        }
+    });
 }
