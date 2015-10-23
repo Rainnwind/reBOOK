be6b908042f4cfb4bee29b0c9be52725f6e25ba5
Rane Eriksen <re@shipfeed.dk>
2015-10-24 00:06:58 +0200

    Added visual content to bugs pages

--- web/app/pages/application/bugs/controller.js
+++ web/app/pages/application/bugs/controller.js
@@ -1,4 +1,19 @@
 var APP = angular.module("APP");
-APP.controller("bugsController", ["$scope", function($scope) {
-    console.log($scope);
+APP.controller("bugsController", ["$scope", "_bugs", "$stateParams", function($scope, _bugs, $stateParams) {
+    $scope.bugs = {
+        list: _bugs.bugs,
+        eps: 15,
+        begin: 0,
+        cp: parseInt($stateParams.page),
+        mp: 7
+    };
+
+    (function init() {
+        _bugs.get_bugs();
+    })();
+
+    $scope.display_bug = function(bug) {
+        bug.display = !bug.display;
+        console.log(bug);
+    };
 }]);
