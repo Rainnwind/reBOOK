191dc5082ded6bd7282ca5b1537f54880b5ec3a0
Rane Eriksen <re@shipfeed.dk>
2015-10-28 01:50:59 +0100

    Better message handling and now accepts setting headers

--- server/LIB/EXPRESS/response.js
+++ server/LIB/EXPRESS/response.js
@@ -13,13 +13,36 @@ module.exports = function(_request, _response, _next) {
 
 
         _this._ERROR = function(msg) {
-            MESSAGES.push({
-                id: uuid.v1() + uuid.v4(),
-                message: msg,
-                type: "ERROR"
-            });
+            if (typeof msg === "string") {
+                MESSAGES.push({
+                    id: uuid.v1() + uuid.v4(),
+                    message: msg,
+                    type: "ERROR"
+                });
+            } else if (typeof msg === "object") {
+                for (var key in msg.errors) {
+                    if (msg.errors.hasOwnProperty(key)) {
+                        MESSAGES.push({
+                            id: uuid.v1() + uuid.v4(),
+                            message: msg.errors[key].message || "Unknown error",
+                            type: "ERROR"
+                        });
+                    }
+                }
+            } else {
+                MESSAGES.push({
+                    id: uuid.v1() + uuid.v4(),
+                    message: "Unknown error",
+                    type: "ERROR"
+                });
+            }
             return _this;
-        }
+        };
+
+        _this._HEADER = function(key, value) {
+            _response_.setHeader(key, value);
+            return _this;
+        };
 
         _this._INFO = function(msg) {
             MESSAGES.push({
@@ -28,7 +51,7 @@ module.exports = function(_request, _response, _next) {
                 type: "INFO"
             });
             return _this;
-        }
+        };
 
         _this._SUCCESS = function(msg) {
             MESSAGES.push({
@@ -37,17 +60,17 @@ module.exports = function(_request, _response, _next) {
                 type: "SUCCESS"
             });
             return _this;
-        }
+        };
 
         _this._DATA = function(key, object) {
             RESPONSE[key] = object;
             return _this;
-        }
+        };
 
         _this._STATUS = function(statusCode) {
             _response_.status(statusCode);
             return _this;
-        }
+        };
 
 
         _this._send = function() {
