aabb17a777d987fe1fcd2f852df83c31516ed172
Rane Eriksen <re@shipfeed.dk>
2015-10-29 00:49:32 +0100

    It now opens a modal to translate in

--- web/app/components/comp-language/controller.js
+++ web/app/components/comp-language/controller.js
@@ -23,7 +23,8 @@ APP.directive("compLanguage", [function() {
 
             $scope.open_translation = function($event) {
                 if ($($event.target).hasClass("admin-tool-gear")) {
-                    alert("OPEN THE FUCKER123");
+                    _language.set_translation_language($scope.base);
+                    $("#modal-language").modal("toggle");
                 }
             };
 
