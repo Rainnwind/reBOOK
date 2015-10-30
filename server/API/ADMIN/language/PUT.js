ab0b34552cfca3b523d995a68b6393e94551df04
Rane Eriksen <re@shipfeed.dk>
2015-10-30 22:41:05 +0100

    Added multiple languages

--- server/API/ADMIN/language/PUT.js
+++ server/API/ADMIN/language/PUT.js
@@ -5,8 +5,47 @@ module.exports = function(_request, _response) {
         base: _request.body.base
     }, {
         $set: {
+            arabic: _request.body.arabic,
+            bulgarian: _request.body.bulgarian,
+            catalan: _request.body.catalan,
+            chinese: _request.body.chinese,
+            croatian: _request.body.croatian,
+            czech: _request.body.czech,
+            danish: _request.body.danish,
+            dutch: _request.body.dutch,
             english: _request.body.english,
-            danish: _request.body.danish
+            estonian: _request.body.estonian,
+            finnish: _request.body.finnish,
+            french: _request.body.french,
+            german: _request.body.german,
+            greek: _request.body.greek,
+            hebrew: _request.body.hebrew,
+            hindi: _request.body.hindi,
+            hungarian: _request.body.hungarian,
+            icelandic: _request.body.icelandic,
+            indonesian: _request.body.indonesian,
+            italian: _request.body.italian,
+            japanese: _request.body.japanese,
+            korean: _request.body.korean,
+            latvian: _request.body.latvian,
+            lithuanian: _request.body.lithuanian,
+            malay: _request.body.malay,
+            norwegian: _request.body.norwegian,
+            persian: _request.body.persian,
+            polish: _request.body.polish,
+            portuguese: _request.body.portuguese,
+            romanian: _request.body.romanian,
+            russian: _request.body.russian,
+            serbian: _request.body.serbian,
+            slovak: _request.body.slovak,
+            slovenian: _request.body.slovenian,
+            spanish: _request.body.spanish,
+            swedish: _request.body.swedish,
+            thai: _request.body.thai,
+            turkish: _request.body.turkish,
+            ukrainian: _request.body.ukrainian,
+            urdu: _request.body.urdu,
+            vietnamese: _request.body.vietnamese
         }
     }, function(err, body) {
         if (!err) {
--- web/app/components/modal-language/controller.js
+++ web/app/components/modal-language/controller.js
@@ -2,8 +2,69 @@ var APP = angular.module("APP");
 APP.directive("modalLanguage", [function() {
     return {
         templateUrl: "components/modal-language/template.html",
-        controller: ["$scope", "_notifications", "_language", function($scope, _notifications, _language) {
+        controller: ["$scope", "_notifications", "_language", "_load", "_user", function($scope, _notifications, _language, _load, _user) {
             $scope._language = _language;
+
+            $scope.translate_with_google = function($event, base, language) {
+                $event.preventDefault(); //We do not want form to submet
+
+                var current_language = $scope._language.translate_language;
+
+                $scope.google_languages = {
+                    arabic: "ar",
+                    bulgarian: "bg",
+                    catalan: "ca",
+                    chinese: "zh_CN",
+                    croatian: "hr",
+                    czech: "cs",
+                    danish: "da",
+                    dutch: "nl",
+                    english: "en",
+                    estonian: "et",
+                    finnish: "fi",
+                    french: "fr",
+                    german: "de",
+                    greek: "el",
+                    hebrew: "iw",
+                    hindi: "hi",
+                    hungarian: "hu",
+                    icelandic: "is",
+                    indonesian: "id",
+                    italian: "it",
+                    japanese: "ja",
+                    korean: "ko",
+                    latvian: "lv",
+                    lithuanian: "lt",
+                    malay: "ms",
+                    norwegian: "no",
+                    persian: "fa",
+                    polish: "pl",
+                    portuguese: "pt",
+                    romanian: "ro",
+                    russian: "ru",
+                    serbian: "sr",
+                    slovak: "sk",
+                    slovenian: "sl",
+                    spanish: "es",
+                    swedish: "sv",
+                    thai: "th",
+                    turkish: "tr",
+                    ukrainian: "uk",
+                    urdu: "ur",
+                    vietnamese: "vi"
+                };
+
+
+
+                _load.get("https://www.googleapis.com/language/translate/v2?key=" + _user.user.api_keys.language + "&target=" + $scope.google_languages[language] + "&q=" + encodeURIComponent(base))
+                    .then(function(result) {
+                        current_language[language] = result.data.data.translations[0].translatedText;
+                    })
+                    .catch(function(err) {
+                        _notifications.ERROR("Failed to translate language");
+                        console.trace(err);
+                    });
+            };
             $scope.submit = function() {
                 _language.save_language($scope._language.translate_language)
                     .then(function() {
